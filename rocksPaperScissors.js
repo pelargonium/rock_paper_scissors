8d2c0bad3f9dbdec0ba003ed4b52306609a906eb
Karl B <karlbeyer@icloud.com>
2024-08-26 00:42:06 -0400

    Added functional UI buttons, refactored playRound to trigger on button click

--- rocksPaperScissors.js
+++ rocksPaperScissors.js
@@ -1,28 +1,81 @@
 let humanScore = 0; //define players score variable
 let computerScore = 0; //define computer score variable
+let playerChoice = "";
+let computerChoice = "";
 
-playGame(humanScore,computerScore);
+// playGame(humanScore,computerScore); //run the game
 
-function playGame(human,computer)
-{
-do
-  {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    playRound(computerChoice, humanChoice);
-  }
-  while (humanScore <= 2 && computerScore <= 2);
+let menu = document.querySelector("#playerChoice");
+console.log(menu);
+menu.addEventListener('click', (event) => {
+  let target = event.target;
 
-  if (humanScore > computerScore) 
-  {
-    console.log("You won the game!");
+  switch(target.id) {
+      case 'rock':
+          console.log('Rock was clicked');
+          playerChoice = "rock";
+          console.log(playerChoice);
+          computerChoice = getComputerChoice();
+          console.log(computerChoice);
+          playRound(computerChoice,playerChoice);
+          cleanup();
+          break;
+      case 'paper':
+          console.log('Paper was clicked');
+          playerChoice = "paper";
+          console.log(playerChoice);
+          computerChoice = getComputerChoice();
+          console.log(computerChoice);
+          playRound(computerChoice,playerChoice);
+          cleanup();
+          break;
+      case 'scissors':
+          console.log('Scissors was clicked');
+          playerChoice = "scissors";
+          console.log(playerChoice);
+          computerChoice = getComputerChoice();
+          console.log(computerChoice);
+          playRound(computerChoice,playerChoice);
+          cleanup();
+          break;
   }
-  else
-  {
-    console.log("You lost the game!");
-  }
-  return;
+});
+
+function cleanup() {
+  console.log(`Player: ${humanScore}, Computer: ${computerScore}`);
+          playerChoice ="";
+          computerChoice ="";
 }
+
+//on button click
+  //save player choice to humanChoice
+  //get computer choice
+  //compare input
+  //display outcome
+  //change score
+  //check for win conditions
+  //reset humanChoice
+
+// function playGame(human,computer)
+                                                     
+
+//     let computerChoice = getComputerChoice();
+//     console.log(computerChoice);
+
+//     playRound(computerChoice, humanChoice);
+
+//   while (humanScore <= 1 && computerScore <= 1);
+
+//   if (humanScore > computerScore) 
+//   {
+//     console.log("You won the game!");
+//   }
+//   else
+//   {
+//     console.log("You lost the game!");
+//   }
+//   return;
+// }
 
 function getComputerChoice() {  //Get computer choice
   let number = Math.random() * 100; //use Math.random * 100 to generate a number
@@ -35,23 +88,30 @@ function getComputerChoice() {  //Get computer choice
 }
 
 
-function getHumanChoice() 
-{   //define function getHumanChoice
-  let output;
-  while (output === undefined) //loop until valid input
-    {
-    let input = prompt("Type rock, paper, or scissors:");  //use prompt method to get user input
-    if (input.toLowerCase() === "rock" || input.toLowerCase() === "paper" || input.toLowerCase() === "scissors") //IF user input is rock, paper or scissors 
-      {
-      output = input.toLowerCase();
-      return output; // return humanChoice
-      }
-    else 
-      {
-      console.log("Invalid entry. Try again!" )//else display wrong input error and repeat prompt
-      }
-    }
-}
+
+  // let rock = document.getElementById("rock");
+  // let paper = document.getElementById("paper");
+  // let scissors = document.getElementById("scissors");
+
+
+  // rock.addEventListener("click", function(event){output = "rock"});
+  // paper.addEventListener("click", function(event){output = "paper"});
+  // scissors.addEventListener("click", function(event){output = "scissors"});
+
+
+
+    // let input = prompt("Type rock, paper, or scissors:");  //use prompt method to get user input
+    // if (input.toLowerCase() === "rock" || input.toLowerCase() === "paper" || input.toLowerCase() === "scissors") //IF user input is rock, paper or scissors 
+    //   {
+    //   output = input.toLowerCase();
+    //   return output; // return humanChoice
+    //   }
+    // else 
+    //   {
+    //   console.log("Invalid entry. Try again!" )//else display wrong input error and repeat prompt
+    //   }
+// }
+
 
 function playRound(computer, human) //define function playRound that accepts two parameters
 {
